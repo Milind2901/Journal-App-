fe6b8efc3fe9b439e2f0ac5d6c5d9a35adc3099a
Milind2901 <timchaturvedi29@gmail.com>
2026-04-27 04:35:16 +0530

    JWT Token Auth is added

--- src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/PublicController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController // made a bean with endpoint
